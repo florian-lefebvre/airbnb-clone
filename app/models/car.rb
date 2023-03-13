565e294e01e0cf6d5e4f0090f316fd808fecd645
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-03-13 17:08:46 +0100

    create migration and model; adapt user model

---
+++ app/models/car.rb
@@ -0,0 +1,7 @@
+class Car < ApplicationRecord
+  belongs_to :user
+
+  validates :car, presence: true, inclusion: { in: 1960..Date.today.year }, numericality: { only_integer: true }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 1.0 }
+  validates :user, presence: true
+end
