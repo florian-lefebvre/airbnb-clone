583e07e07c1ba350bb811bec8a0077a79c3f3b33
Maria Skrabalekova <43684758+marieSK@users.noreply.github.com>
2023-03-14 12:29:07 +0100

    Update app/models/booking.rb
    Co-authored-by: Florian LEFEBVRE <contact@florian-lefebvre.dev>

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,16 @@ class Booking < ApplicationRecord
   validates :start_date, :end_date, presence: true, inclusion: { in: Date.today.. }
   validates :end_date, comparison: { greater_than: :start_date }
   validates :status, inclusion: %w[accepted pending rejected]
+  
+  def accepted?
+      status == "accepted" 
+  end
+  
+  def pending?
+      status == "pending" 
+  end
+  
+  def rejected?
+      status == "rejected" 
+  end
 end
