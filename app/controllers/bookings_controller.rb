a89ed96234bbebe40e8d09587f530f533c027d9e
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-03-14 17:25:55 +0100

    Update bookings_controller.rb

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,7 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: [:show]
+  before_action :set_car, only: %i[create]
+  before_action :set_user, only: %i[create]
 
   def index
     @bookings = Booking.all
@@ -7,20 +9,26 @@ class BookingsController < ApplicationController
 
   def show
   end
+  
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.car = @car
+    @booking.user = @user
+    if @booking.save
+      redirect_to car_path(@car)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
 
-  def update_params
-    params.require(:booking).permit(:accepted)
-  end
-
-  def renter_booking_params
-    params.require(:booking).permit(:from, :to)
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 
   def set_user
     @user = current_user
-    # @user = User.find(params[:user_id])
   end
 
   def set_car
