ac67e0b539b7a689900cf812821b9b0fe9cc4fe9
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-03-15 13:05:03 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+require "open-uri"
+
+5.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = Faker::Internet.email(name: "#{first_name} #{last_name}")
+  password = Faker::Internet.password(min_length: 6)
+  user = User.create(email:, password:, first_name:, last_name:)
+  puts user.full_name
+  rand(1..5).times do
+    year = Faker::Vehicle.year
+    price = rand(50.0..1000.0)
+    kilometers = rand(10_000..300_000)
+    brand = Faker::Vehicle.make
+    model = "#{brand} #{Faker::Vehicle.model(make_of_model: brand)}"
+    seats = rand(4..5)
+    color = Faker::Color.color_name
+    car_type = Faker::Vehicle.car_type
+    file = URI.open(Faker::LoremFlickr.image(size: "1200x800", search_terms: ["car"]))
+
+    car = Car.new(year:, price:, kilometers:, model:, seats:, color:, car_type:, user:)
+    car.photo.attach(io: file, filename: "#{model}.png", content_type: "image/jpeg")
+    car.save
+
+    puts car.model
+  end
+end
