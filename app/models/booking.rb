14539a51964397328f63e1a52295c9d4e5cdba4a
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-03-16 12:28:13 +0100

    fix validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,8 +3,8 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   validates :start_date, presence: true
-  validates :start_date, inclusion: { in: Date.today.. }, if: -> { id.nil? }
-  validates :end_date, presence: true, comparison: { greater_than: :start_date }
+  validate :validate_start_date
+  validates :end_date, presence: true, comparison: { greater_than_or_equal_to: :start_date }
   validates :status, inclusion: %w[accepted pending rejected]
 
   def accepted?
@@ -17,5 +17,16 @@ class Booking < ApplicationRecord
 
   def rejected?
     status == "rejected"
+  end
+
+  private
+
+  def validate_start_date
+    creating = id.nil?
+    has_error = false
+    has_error = true if creating && start_date < Date.today
+    has_error = false if !creating && start_date == start_date_was
+    has_error = true if !creating && start_date != start_date_was && start_date < Date.today
+    errors.add(:start_date, "can't be in the past") if has_error
   end
 end
