55d4cbd024419e862bebac78e9b5f6c040bd1348
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-03-16 11:39:41 +0100

    test

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :car
   belongs_to :user
 
-  validates :start_date, :end_date, presence: true, inclusion: { in: Date.today.. }
-  validates :end_date, comparison: { greater_than: :start_date }
+  validates :start_date, presence: true
+  validates :start_date, inclusion: { in: Date.today.. }, if: -> { id.nil? }
+  validates :end_date, presence: true, comparison: { greater_than: :start_date }
   validates :status, inclusion: %w[accepted pending rejected]
 
   def accepted?
