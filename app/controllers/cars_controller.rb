609e3ae01396a01b46df0cf08e6ef3f5b52ad5d6
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-03-16 14:28:34 +0100

    rework

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -17,6 +17,7 @@ class CarsController < ApplicationController
 
   def show
     @booking = Booking.new
+    @bookings = @car.bookings.filter { |b| b.end_date > Date.today && b.accepted? }
     authorize @car
   end
 
